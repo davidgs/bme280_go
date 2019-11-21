d49eb15af703e818b57df79f0eec732df2c69f0a
David G. Simmons <santafen@mac.com>
2019-11-21 12:23:33 -0500

    Refactored to match c++ library

--- bme280.go
+++ bme280.go
@@ -2,25 +2,85 @@ package bme280_go
 
 import (
 	"fmt"
-
 	"time"
-
+	"math"
 	"golang.org/x/exp/io/i2c"
 )
 
 // BME280 holdsw the device and all configuration data for the sensor
 type BME280 struct {
 	Dev            *i2c.Device
+	Config         bme280Config
 	tempressConfig [3]int
 	pressConfig    [9]int
 	hConfig        [6]int
+	tFine			int32
 }
+const (
+    bme280RegisterDigT1 = 0x88
+	bme280RegisterDigT2 = 0x8A
+	bme280RegisterDigT3 = 0x8C
+  
+	bme280RegisterDigP1 = 0x8E
+	bme280RegisterDigP2 = 0x90
+	bme280RegisterDigP3 = 0x92
+	bme280RegisterDigP4 = 0x94
+	bme280RegisterDigP5 = 0x96
+	bme280RegisterDigP6 = 0x98
+	bme280RegisterDigP7 = 0x9A
+	bme280RegisterDigP8 = 0x9C
+	bme280RegisterDigP9 = 0x9E
+  
+	bme280RegisterDigH1 = 0xA1
+	bme280RegisterDigH2 = 0xE1
+	bme280RegisterDigH3 = 0xE3
+	bme280RegisterDigH4 = 0xE4
+	bme280RegisterDigH5 = 0xE5
+	bme280RegisterDigH6 = 0xE7
+  
+	bme280RegisterChipID = 0xD0
+	bme280RegisterVersion = 0xD1
+	bme280RegisterSoftReset = 0xE0
+  
+	bme280RegisterCal26 = 0xE1 // R calibration stored in 0xE1-0xF0
+  
+	bme280RegisterControlHumid = 0xF2
+	bme280RegisterStatus = 0xF3
+	bme280RegisterControl= 0xF4
+	bme280RegisterConfig = 0xF5
+	bme280RegisterPressureData = 0xF7
+	bme280RegisterTempData = 0xFA
+	bme280RegisterHumidData = 0xFD
+)
 
+type bme280Config struct {
+	digT1 uint16///< temperature compensation value
+	digT2 uint16 ///< temperature compensation value
+	digT3 uint16 ///< temperature compensation value
+  
+	digP1 uint16///< pressure compensation value
+	digP2 uint16 ///< pressure compensation value
+	digP3 uint16 ///< pressure compensation value
+	digP4 uint16  ///< pressure compensation value
+	digP5 uint16  ///< pressure compensation value
+	digP6 uint16  ///< pressure compensation value
+	digP7 uint16  ///< pressure compensation value
+	digP8 uint16  ///< pressure compensation value
+	digP9 uint16  ///< pressure compensation value
+  
+	digH1 uint8 ///< humidity compensation value
+	digH2 uint16 ///< humidity compensation value
+	digH3 uint8 ///< humidity compensation value
+	digH4 uint16 ///< humidity compensation value
+	digH5 uint16 ///< humidity compensation value
+	digH6 uint8  ///< humidity compensation value
+  }
 // BMEData contains the read-values from the sensor
 type BMEData struct {
-	Temperature int
-	Humidity    int
-	Pressure    int
+	Temperature float32
+	Humidity    float32
+	Pressure    float32
+	Altitude	float32
 }
 
 //
@@ -29,20 +89,14 @@ type BMEData struct {
 // into auto sensing mode
 //
 func (bme280 *BME280) BME280Init(channel string) int {
-	//device := BME280{}
-	//bme280.tempressConfig = make([]int, 3)
-	//bme280.pressConfig = make([]int, 9)
-	//bme280.hConfig = make([]int, 6)
-	ucCal := make([]byte, 36)
 	var err error
 	bme280.Dev, err = i2c.Open(&i2c.Devfs{Dev: channel}, 0x77)
 	if err != nil {
 		panic(err)
 	}
-	//defer device.dev.Close()
 	// get ID
 	b := []byte{0x00}
-	err = bme280.Dev.ReadReg(0xD0, b)
+	err = bme280.Dev.ReadReg(bme280RegisterChipID, b)
 	if err != nil {
 		fmt.Println("Chip ID Err ", err)
 		return -256
@@ -51,7 +105,7 @@ func (bme280 *BME280) BME280Init(channel string) int {
 		fmt.Println("Invalid chip ID! ", b)
 		return -256
 	}
-	cB := []byte{0xE0, 0xB6}
+	cB := []byte{bme280RegisterSoftReset, 0xB6}
 
 	err = bme280.Dev.Write(cB)
 	if err != nil {
@@ -59,7 +113,17 @@ func (bme280 *BME280) BME280Init(channel string) int {
 		return -256
 	}
 	time.Sleep(300 * time.Millisecond)
+// if chip is still reading calibration, delay
+    for {
+		breaker :=  bme280.isReadingCalibration() 
+	 	if(breaker){
+			time.Sleep(100 * time.Millisecond)
+		} else {
+			break
+		}
+	}
 
+    bme280.readCoefficients(); // read trimming parameters, see DS 4.2.2
 	calib1 := make([]byte, 24)
 	// Read 24 bytes of calibration data
 	err = bme280.Dev.ReadReg(0x88, calib1)
@@ -68,180 +132,293 @@ func (bme280 *BME280) BME280Init(channel string) int {
 		fmt.Println("calibration data not read correctly", err)
 		return -256
 	}
-	x := 0
-	for x < 24 {
-		ucCal[x] = calib1[x]
-		x++
-	}
-	b = []byte{0x00}
-	// get humidity calibration byte
-	err = bme280.Dev.ReadReg(0xA1, b)
+	return 0
+} /* bme280Init() */
+
+func  (bme280 *BME280) isReadingCalibration() bool {
+	var rStatus []byte
+	err := bme280.Dev.ReadReg(bme280RegisterStatus, rStatus);
 	if err != nil {
-		fmt.Println("Failed to read humidity calibration byte", err)
+		fmt.Println("Register read error: ", err)
+		return false
+	}
+	const ft = uint8(1 << 0)
+	return (uint8(rStatus[0]) & ft) != 0
+}
+
+func (bme280 *BME280) readCoefficients() int {
+	var readByte []byte
+	err := bme280.Dev.ReadReg(bme280RegisterDigT1, readByte)
+	
+	if err != nil {
+		fmt.Println("Configuration read error: ", err)
 		return -256
 	}
-	ucCal[x] = b[0]
-	x++
-	calib2 := make([]byte, 7)
-	err = bme280.Dev.ReadReg(0xE1, calib2)
-	if err != nil {
-		fmt.Println("Failed to read humiduty calibration byte 2: ", err)
-		return -256
-	}
-	y := 0
-	for x < 32 {
-		ucCal[x] = calib2[y]
-		x++
-		y++
-	}
-	// time to set up the calibration
-	bme280.tempressConfig[0] = int(ucCal[0]) + (int(ucCal[1]) << 8)
-	bme280.tempressConfig[1] = int(ucCal[2]) + (int(ucCal[3]) << 8)
-	if bme280.tempressConfig[1] > 32767 {
-		bme280.tempressConfig[1] -= 65536
-	}
-	bme280.tempressConfig[2] = int(ucCal[4]) + (int(ucCal[5]) << 8)
-	if bme280.tempressConfig[2] > 32767 {
-		bme280.tempressConfig[2] -= 65536
-	}
-	// Prepare pressure calibration data
-	bme280.pressConfig[0] = int(ucCal[6]) + (int(ucCal[7]) << 8)
-	bme280.pressConfig[1] = int(ucCal[8]) + (int(ucCal[9]) << 8)
-	if bme280.pressConfig[1] > 32767 {
-		bme280.pressConfig[1] -= 65536
-	}
-	bme280.pressConfig[2] = int(ucCal[10]) + (int(ucCal[11]) << 8)
-	if bme280.pressConfig[2] > 32767 {
-		bme280.pressConfig[2] -= 65536
-	}
-	bme280.pressConfig[3] = int(ucCal[12]) + (int(ucCal[13]) << 8)
-	if bme280.pressConfig[3] > 32767 {
-		bme280.pressConfig[3] -= 65536
-	}
-	bme280.pressConfig[4] = int(ucCal[14]) + (int(ucCal[15]) << 8)
-	if bme280.pressConfig[4] > 32767 {
-		bme280.pressConfig[4] -= 65536
-	}
-	bme280.pressConfig[5] = int(ucCal[16]) + (int(ucCal[17]) << 8)
-	if bme280.pressConfig[5] > 32767 {
-		bme280.pressConfig[5] -= 65536
-	}
-	bme280.pressConfig[6] = int(ucCal[18]) + (int(ucCal[19]) << 8)
-	if bme280.pressConfig[6] > 32767 {
-		bme280.pressConfig[6] -= 65536
-	}
-	bme280.pressConfig[7] = int(ucCal[20]) + (int(ucCal[21]) << 8)
-	if bme280.pressConfig[7] > 32767 {
-		bme280.pressConfig[7] -= 65536
-	}
-	bme280.pressConfig[8] = int(ucCal[22]) + (int(ucCal[23]) << 8)
-	if bme280.pressConfig[8] > 32767 {
-		bme280.pressConfig[8] -= 65536
-	}
-	// Prepare humidity calibration data
-	bme280.hConfig[0] = int(ucCal[24])
-	bme280.hConfig[1] = int(ucCal[25]) + (int(ucCal[26]) << 8)
-	if bme280.hConfig[1] > 32767 {
-		bme280.hConfig[1] -= 65536
-	}
-	bme280.hConfig[2] = int(ucCal[27])
-	bme280.hConfig[3] = (int(ucCal[28]) << 4) + (int(ucCal[29]) & 0xf)
-	if bme280.hConfig[3] > 2047 {
-		bme280.hConfig[3] -= 4096
-	}
-	bme280.hConfig[4] = (int(ucCal[30]) << 4) + (int(ucCal[29]) >> 4)
-	if bme280.hConfig[4] > 2047 {
-		bme280.hConfig[4] -= 4096
-	}
-	bme280.hConfig[5] = int(ucCal[31])
-	if bme280.hConfig[5] > 127 {
-		bme280.hConfig[5] -= 256
-	}
-	tB := []byte{0xF2, 0x01}
-	err = bme280.Dev.Write(tB)
-	if err != nil {
-		fmt.Println("Humidity control error: ", err)
-		return -256
-	}
-	tB[0] = 0xF4
-	tB[1] = 0x27
-	err = bme280.Dev.Write(tB)
-	if err != nil {
-		fmt.Println("Measurement mode set error: ", err)
-		return -256
-	}
-	tB[0] = 0xF5
-	tB[1] = 0xA0
-	err = bme280.Dev.Write(tB)
+	bme280.Config.digT1 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigT2, readByte)
 	if err != nil {
 		fmt.Println("Configuration write error: ", err)
 		return -256
 	}
+	bme280.Config.digT2 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigT3, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digT3 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP1, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP1 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP2, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP2 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP3, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP3 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP4, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP4 = uint16(readByte[0] | readByte[0])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP5, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP5 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP6, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP6 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP7, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP7 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP8, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digP8 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigP9, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+  	bme280.Config.digP9 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigH1, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digH1 = uint8(readByte[0])
+	err = bme280.Dev.ReadReg(bme280RegisterDigH2, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digH2 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigH3, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digH3 = uint8(readByte[0])
+	err = bme280.Dev.ReadReg(bme280RegisterDigH4, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+	}
+	temp := readByte[0]
+	bme280.Config.digH4 = uint16(readByte[0] | readByte[1])
+	err = bme280.Dev.ReadReg(bme280RegisterDigH4 + 1, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+	}
+	bme280.Config.digH4 = uint16(temp << 4 | readByte[0] & 0xF)
+	err = bme280.Dev.ReadReg(bme280RegisterDigH5 + 1, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+	}
+	temp = readByte[0]
+	err = bme280.Dev.ReadReg(bme280RegisterDigH5, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+	}
+	bme280.Config.digH5 = uint16(temp << 4 | readByte[0] >> 4);
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	err = bme280.Dev.ReadReg(bme280RegisterDigH6, readByte)
+	if err != nil {
+		fmt.Println("Configuration write error: ", err)
+		return -256
+	}
+	bme280.Config.digH6 = uint8(readByte[0])
 	return 0
-} /* bme280Init() */
-
+  }
+  
 //
-// BME280ReadValues Reads the sensor register values
+// bme280ReadValues Reads the sensor register values
 // and translate them into calibrated readings
 // using the previously loaded calibration data
 // Temperature is expressed in Celsius degrees as T * 100 (for 2 decimal places)
 // Pressure is <future>
 // Humidity is express as H * 1024 (10 bit fraction)
 //
-func (bme280 *BME280) BME280ReadValues() BMEData {
+func (bme280 *BME280) bme280ReadValues() BMEData {
 	data := BMEData{}
-	ret := make([]byte, 8)
-	// r := BMEData{}
-
-	err := bme280.Dev.ReadReg(0xF7, ret)
-	if err != nil {
-		fmt.Println("Failed to read Data: ", err)
-		return data
-	}
-	p := (int(ret[0]) << 12) + (int(ret[1]) << 4) + (int(ret[2]) >> 4)
-	t := (int(ret[3]) << 12) + (int(ret[4]) << 4) + (int(ret[5]) >> 4)
-	h := (int(ret[6]) << 8) + int(ret[7])
-	//fmt.Println("Raw values: ", p, t, h)
-	// Calculate calibrated temperature value
-	// the value is 100x C (e.g. 2601 = 26.01C)
-	var1 := (((t >> 3) - (bme280.tempressConfig[0] << 1)) * (bme280.tempressConfig[1])) >> 11
-	var2 := (((((t >> 4) - (bme280.tempressConfig[1])) * ((t >> 4) - (bme280.tempressConfig[0]))) >> 12) * (bme280.tempressConfig[2])) >> 14
-	tFine := var1 + var2
-	T := (tFine*5 + 128) >> 8
-	fmt.Println("Calibrated Temp: ", T)
-	// Calculate calibrated humidity value
-	var1 = (tFine - 76800)
-	var1 = (((((h << 14) - ((bme280.hConfig[3]) << 20) - ((bme280.hConfig[4]) * var1)) +
-		(16384)) >> 15) * (((((((var1*(bme280.hConfig[5]))>>10)*(((var1*(bme280.hConfig[2]))>>11)+(32768)))>>10)+(2097152))*(bme280.hConfig[1]) + 8192) >> 14))
-	var1 = (var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * (bme280.hConfig[0])) >> 4))
-	if var1 < 0 {
-		var1 = 0
-	}
-	if var1 > 419430400 {
-		var1 = 419430400
-	}
-	H := var1 >> 12
-	P := 0
-	// Calculate calibrated pressure value
-	var1_64 := uint64(tFine - 128000)
-	var2_64 := uint64(var1_64 * var1_64 * uint64(bme280.pressConfig[5]))
-	var2_64 = var2_64 + ((var1_64 * uint64(bme280.pressConfig[4])) << 17)
-	var2_64 = var2_64 + ((uint64(bme280.pressConfig[3])) << 35)
-	var1_64 = ((var1_64 * var1_64 * uint64(bme280.pressConfig[2])) >> 8) + ((var1_64 * uint64(bme280.pressConfig[1])) << 12)
-	var1_64 = (((1) << 47) + var1_64) * (uint64(bme280.pressConfig[0])) >> 33
-	if var1_64 == 0 {
-		P = 0
-	} else {
-		p64 := uint64(1048576 - p)
-		p64 = (((p64 << 31) - var2_64) * 3125) / var1_64
-		var1_64 = ((uint64(bme280.pressConfig[8])) * (p64 >> 13) * (p64 >> 13)) >> 25
-		var2_64 = ((uint64(bme280.pressConfig[7])) * p64) >> 19
-		p64 = ((p64 + var1_64 + var2_64) >> 8) + ((uint64(bme280.pressConfig[6])) << 4)
-		P = int(p64 / 100)
-	}
-
-	data.Temperature = T
-	data.Pressure = P
-	data.Humidity = H
+	
+	data.Temperature = bme280.BME280ReadTemperature()
+	data.Pressure = bme280.BME280ReadPressure()
+	data.Humidity = bme280.BME280ReadHumidity()
+	data.Altitude = bme280.BME280ReadAltitude(1013.25)
+		
 	return data
 }
+
+func (bme280 *BME280) BME280ReadTemperature() float32 {
+	var var1, var2, adcT int32
+    var readByte []byte
+	bme280.Dev.ReadReg(bme280RegisterTempData, readByte)
+    adcT = int32(readByte[0])
+	adcT = adcT | int32(readByte[1])
+    adcT = adcT << 8;
+	adcT = adcT | int32(readByte[2] )
+	if adcT == 0x800000 { // value in case temp measurement was disabled
+	  return -256.00;
+	}
+	adcT >>= 4;
+  
+	var1 = ((((adcT >> 3) - (int32(bme280.Config.digT1 << 1)))) *
+			(int32(bme280.Config.digT2))) >> 11
+  
+	var2 = (((((adcT >> 4) - (int32(bme280.Config.digT1))) *
+			  ((adcT >> 4) - (int32(bme280.Config.digT1)))) >> 12) *
+			(int32(bme280.Config.digT3))) >> 14
+  
+	bme280.tFine = var1 + var2
+  
+	var T = float32((bme280.tFine * 5 + 128) >> 8)
+	return T / 100.00
+  }
+
+  func (bme280 *BME280) BME280ReadPressure() float32 {
+	var var1, var2, p int64
+  
+	_ = bme280.BME280ReadTemperature(); // must be done first to get t_fine
+	var readByte []byte
+	bme280.Dev.ReadReg(bme280RegisterPressureData, readByte)
+    var adcP = int32(readByte[0])
+	adcP = adcP | int32(readByte[1])
+    adcP = adcP << 8;
+	adcP = adcP | int32(readByte[2] )
+	if adcP == 0x800000 { // value in case pressure measurement was disabled
+	  return -256.00
+	}
+	adcP >>= 4
+  
+	var1 = (int64(bme280.tFine)) - 128000
+	var2 = var1 * var1 * int64(bme280.Config.digP6)
+	var2 = var2 + ((var1 * int64(bme280.Config.digP5)) << 17);
+	var2 = var2 + ((int64(bme280.Config.digP4)) << 35)
+	var1 = ((var1 * var1 * int64(bme280.Config.digP3)) >> 8) +
+		   ((var1 * int64(bme280.Config.digP2)) << 12)
+	var1 =
+		((((int64(1)) << 47) + var1)) * (int64(bme280.Config.digP1)) >> 33
+  
+	if (var1 == 0) {
+	  return 0; // avoid exception caused by division by zero
+	}
+	p = int64(1048576 - adcP)
+	p = (((p << 31) - var2) * 3125) / var1
+	var1 = ((int64(bme280.Config.digP9)) * (p >> 13) * (p >> 13)) >> 25
+	var2 = ((int64(bme280.Config.digP8)) * p) >> 19
+  
+	p = ((p + var1 + var2) >> 8) + ((int64(bme280.Config.digP7)) << 4)
+	return float32(p / 256)
+  }
+  func (bme280 *BME280) BME280ReadHumidity() float32 {
+	_ = bme280.BME280ReadTemperature(); // must be done first to get t_fine
+	var adcH int32 
+	var readByte []byte
+	bme280.Dev.ReadReg(bme280RegisterHumidData, readByte)
+    adcH = int32(readByte[0])
+	adcH = adcH | int32(readByte[1])
+    adcH = adcH << 8;
+	adcH = adcH | int32(readByte[2] )
+	if adcH == 0x8000{ // value in case humidity measurement was disabled
+	  return -256.00
+	}
+  
+	var vx1u32r int32
+  
+	vx1u32r = (bme280.tFine - (int32(76800)))
+  
+	vx1u32r = (((((adcH << 14) - ((int32(bme280.Config.digH4)) << 20) -
+					((int32(bme280.Config.digH5)) * vx1u32r)) +
+				   (int32(16384))) >> 15) *
+				 (((((((vx1u32r * (int32(bme280.Config.digH6))) >> 10) *
+					  (((vx1u32r * (int32(bme280.Config.digH3))) >> 11) +
+					   (int32(32768)))) >> 10) +
+					(int32(2097152))) *
+					   (int32(bme280.Config.digH2)) + 8192) >> 14))
+  
+	vx1u32r = (vx1u32r - (((((vx1u32r >> 15) * (vx1u32r >> 15)) >> 7) *
+							   (int32(bme280.Config.digH1))) >> 4));
+  
+	if vx1u32r < 0 {
+		vx1u32r = 0
+	}
+	if vx1u32r > 419430400 {
+		vx1u32r = 419430400
+	}
+	var h = (vx1u32r >> 12);
+	return float32(h) / 1024.0;
+  }
+
+  func (bme280 *BME280) BME280ReadAltitude(seaLevel float32) float32 {
+	// Equation taken from BMP180 datasheet (page 16):
+	//  http://www.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf
+  
+	// Note that using the equation from wikipedia can give bad results
+	// at high altitude. See this thread for more information:
+	//  http://forums.adafruit.com/viewtopic.php?f=22&t=58064
+  
+	atmospheric := bme280.BME280ReadPressure() / 100.00;
+	return float32(44330.0 * (1.0 - math.Pow(float64(atmospheric / seaLevel), 0.1903)))
+  }
+  
+  /*!
+   *   Calculates the pressure at sea level (in hPa) from the specified
+   * altitude (in meters), and atmospheric pressure (in hPa).
+   *   @param  altitude      Altitude in meters
+   *   @param  atmospheric   Atmospheric pressure in hPa
+   *   @returns the pressure at sea level (in hPa) from the specified altitude
+   */
+  func (bme280 *BME280) BME280SeaLevelForAltitude( altitude float32, atmospheric float32) float32 {
+	// Equation taken from BMP180 datasheet (page 17):
+	//  http://www.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf
+  
+	// Note that using the equation from wikipedia can give bad results
+	// at high altitude. See this thread for more information:
+	//  http://forums.adafruit.com/viewtopic.php?f=22&t=58064
+  
+	return float32(float64(atmospheric) / math.Pow(float64(1.0 - (altitude / 44330.0)), 5.255))
+  }
+  
